8ef7a8ca85fd170bb59e0576e8349ab746dc8e7c
Kundai Dziwa <kdziwa@icloud.com>
2024-11-04 22:23:00 -0700

    Kundais branch (#2)
    * Front end base page created, some editing needed
    on the data transfer and access.
    
    * Few changes to the names.
    
    * Front end transaction page and integration version
    0.1.1.
    
    * New Budgets page created, along with code changes
    to the routes list and some comments.

--- server/routes/transaction.js
+++ server/routes/transaction.js
@@ -4,23 +4,23 @@ import express from "express";
 import db from "../db/connection.js";
 
 // This help convert the id from string to ObjectId for the _id.
-import { ObjectId } from "mongodb";
+import { deserialize, ObjectId } from "mongodb";
 
 // router is an instance of the express router.
 // We use it to define our routes.
-// The router will be added as a middleware and will take control of requests starting with path /record.
+// The router will be added as a middleware and will take control of requests starting with path /transaction.
 const router = express.Router();
 
-// This section will help you get a list of all the records.
+// This section will help you get a list of all the transactions.
 router.get("/", async (req, res) => {
-  let collection = await db.collection("records");
+  let collection = await db.collection("transactions");
   let results = await collection.find({}).toArray();
   res.send(results).status(200);
 });
 
-// This section will help you get a single record by id
+// This section will help you get a single transaction by id
 router.get("/:id", async (req, res) => {
-  let collection = await db.collection("records");
+  let collection = await db.collection("transactions");
   let query = { _id: new ObjectId(req.params.id) };
   let result = await collection.findOne(query);
 
@@ -28,56 +28,56 @@ router.get("/:id", async (req, res) => {
   else res.send(result).status(200);
 });
 
-// This section will help you create a new record.
+// This section will help you create a new transaction.
 router.post("/", async (req, res) => {
   try {
     let newDocument = {
-      name: req.body.name,
-      position: req.body.position,
-      level: req.body.level,
+      amount: req.body.amount,
+      description: req.body.description,
+      type: req.body.type,
     };
-    let collection = await db.collection("records");
+    let collection = await db.collection("transactions");
     let result = await collection.insertOne(newDocument);
     res.send(result).status(204);
   } catch (err) {
     console.error(err);
-    res.status(500).send("Error adding record");
+    res.status(500).send("Error adding transaction");
   }
 });
 
-// This section will help you update a record by id.
+// This section will help you update a transaction by id.
 router.patch("/:id", async (req, res) => {
   try {
     const query = { _id: new ObjectId(req.params.id) };
     const updates = {
       $set: {
-        name: req.body.name,
-        position: req.body.position,
-        level: req.body.level,
+        amount: req.body.amount,
+        description: req.body.description,
+        type: req.body.type,
       },
     };
 
-    let collection = await db.collection("records");
+    let collection = await db.collection("transactions");
     let result = await collection.updateOne(query, updates);
     res.send(result).status(200);
   } catch (err) {
     console.error(err);
-    res.status(500).send("Error updating record");
+    res.status(500).send("Error updating transaction");
   }
 });
 
-// This section will help you delete a record
+// This section will help you delete a transaction
 router.delete("/:id", async (req, res) => {
   try {
     const query = { _id: new ObjectId(req.params.id) };
 
-    const collection = db.collection("records");
+    const collection = db.collection("transactions");
     let result = await collection.deleteOne(query);
 
     res.send(result).status(200);
   } catch (err) {
     console.error(err);
-    res.status(500).send("Error deleting record");
+    res.status(500).send("Error deleting transaction");
   }
 });
 
