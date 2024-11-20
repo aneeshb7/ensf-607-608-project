42229a319d50df2542d78dcfb250d8b5fc99504f
nullified104 <70874090+nullified104@users.noreply.github.com>
2024-11-20 11:07:48 -0700

    Fixed spacing in transaction form and added category field in transaction list (#13)
    * Integrated goals, budget, chatbot with front-end
    
    * Changed api url
    
    * export api
    
    * fixed endpoint calls
    
    * Added debugging statement
    
    * test api url
    
    * Added back old server functionality
    
    * temp change
    
    * Changed logo
    
    * Changed api url
    
    * Added constants file to contain api url
    
    * Update index.html
    
    * Changed gemini model
    
    * Fixed spacing in transaction form and added category field in transaction list
    
    ---------
    
    Co-authored-by: Aneesh Bulusu <aneesh.bulusu@gmail.com>

--- client/src/components/TransactionList.js
+++ client/src/components/TransactionList.js
@@ -21,6 +21,9 @@ const Transaction = (props) => (
       {props.transaction.type}
     </td>
     <td className="p-4 align-middle [&:has([role=checkbox])]:pr-0">
+      {props.transaction.category}
+    </td>
+    <td className="p-4 align-middle [&:has([role=checkbox])]:pr-0">
       <div className="flex gap-2">
         <Link
           className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 h-9 rounded-md px-3"
@@ -113,6 +116,9 @@ export default function TransactionList() {
                 </th>
                 <th className="h-12 px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0">
                   Type
+                </th>
+                <th className="h-12 px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0">
+                  Category
                 </th>
                 <th className="h-12 px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0">
                   Action
